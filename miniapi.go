b4bf5b1b6e3641c109f910bac639c7a4fe4f79d8
moussiam <90825567+moussiam@users.noreply.github.com>
2021-09-16 11:02:23 +0200

    Update miniapi.go
    miniapi

--- miniapi.go
+++ miniapi.go
@@ -13,6 +13,23 @@ func currentHour(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, time.Now().Format("15:04"))
 }
 
+func addToFile(author, entry string) {
+	f, err := os.OpenFile("file.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	_, err2 := f.WriteString(author + ": " + entry + "\n")
+
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+
+	fmt.Println("Enregistré dans le fichier file.txt")
+}
+
 func add(w http.ResponseWriter, req *http.Request) {
 	req.ParseForm()
 	if err := req.ParseForm(); err != nil {
@@ -22,10 +39,10 @@ func add(w http.ResponseWriter, req *http.Request) {
 	}
 	author := req.PostForm.Get("author")
 	entry := req.PostForm.Get("entry")
-  
+
 	fmt.Println(author, ": ", entry)
 	if len(author) > 0 && len(entry) > 0 {
-		saveOnFile(author, entry)
+		addToFile(author, entry)
 		fmt.Fprintf(w, author+": "+entry)
 	} else {
 		fmt.Fprintf(w, "Missing parameters")
@@ -62,7 +79,7 @@ func entries(w http.ResponseWriter, req *http.Request) {
 	for _, entry := range entries {
 		entry := strings.Split(entry, ":")
 
-		fmt.Fprintf(w, entry[1]+"\n")
+		fmt.Fprintf(w, entry[0]+"\n")
 	}
 }
 
